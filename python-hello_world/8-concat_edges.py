675f8b681e9df7484bd09496e81acaea1c3ee03d
KennyGael <k.ishimweg@alustudent.com>
2025-06-18 18:39:49 -0700

    Fix Task 8: No string literals used, only slicing

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[106:112] + str[0:6])
+print(str[39:66] + str[106:107] + str[107:108] + str[108:109] + str[109:110] + str[110:111] + str[0:6])
