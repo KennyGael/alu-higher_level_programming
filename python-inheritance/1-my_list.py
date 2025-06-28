de70ee0ff3e25bd5e5d46f28a90255417c028feb
KennyGael <k.ishimweg@alustudent.com>
2025-06-28 14:11:05 -0700

    Add 1-my_list.py with docstrings and print_sorted method

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""1-my_list module: defines a class MyList that inherits from list"""
+
 class MyList(list):
+    """Custom list class that extends built-in list"""
+
     def print_sorted(self):
+        """Prints the list in ascending sorted order"""
         print(sorted(self))
