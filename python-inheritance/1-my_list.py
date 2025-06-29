4d251e7482a58a867d9c4bc71227bf49bc4054fd
KennyGael <k.ishimweg@alustudent.com>
2025-06-29 11:54:24 -0700

    Fix: Restore print_sorted to print sorted list as required

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -12,12 +12,14 @@ class MyList(list):
 
     def print_sorted(self):
         """
-        Returns the list in ascending sorted order.
+        Prints the list in ascending sorted order.
 
         Example:
         >>> my_list = MyList()
-        >>> my_list.extend([3, 1, 4])
+        >>> my_list.append(3)
+        >>> my_list.append(1)
+        >>> my_list.append(4)
         >>> my_list.print_sorted()
         [1, 3, 4]
         """
-        return sorted(self)
+        print(sorted(self))
