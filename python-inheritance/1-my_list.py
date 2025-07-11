0da414fcafda4b5c3a774b9cba2adedc9f969ccd
KennyGael <k.ishimweg@alustudent.com>
2025-07-11 14:51:45 -0700

    Fix PEP8 issues in 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class MyList that inherits from list."""
 
+
 class MyList(list):
     """A subclass of list with a method to print sorted elements."""
+
     def print_sorted(self):
-        """Prints the list in ascending order without modifying the original list."""
+        """Prints the list in ascending order.
+
+        This method does not modify the original list.
+        """
         print(sorted(self))
