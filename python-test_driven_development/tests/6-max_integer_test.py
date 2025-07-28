bcfb53ca2611b117eae0a4cebc53778f52169276
KennyGael <k.ishimweg@alustudent.com>
2025-07-28 13:18:29 -0700

    Add all mandatory TDD tasks and unittests

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_positive_numbers(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -3, -2]), -1)
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_one_element(self):
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_floats(self):
+        self.assertEqual(max_integer([1.5, 2.4, 0.8]), 2.4)
