e3b8c12d366f489067be1ed204457bb1db9c5dc5
KennyGael <k.ishimweg@alustudent.com>
2025-07-28 12:30:29 -0700

    Fix: send correct header for route validation

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a custom header and displays the body of the response
+# Sends GET request to the URL with custom header
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
